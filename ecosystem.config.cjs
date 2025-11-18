fd53daee281006437f42781a34d3f2380aebbaee
Taurai Job Munodawafa <taujob1111@gmail.com>
2025-11-18 15:12:41 +0200

    Going

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -3,7 +3,7 @@ module.exports = {
     apps: [
       {
         name: 'Lucerna & Stern Healthcare',
-        port: '3023',
+        port: '3021',
         exec_mode: 'cluster',
         instances: '1',
         script: './.output/server/index.mjs' 
